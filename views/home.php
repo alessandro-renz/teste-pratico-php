6660398ee350eb710a755c4a18433425e51e89cb
Alessandro <sandro0325@gmail.com>
2019-01-22 10:32:53 -0200

    topo do site concluido

--- views/home.php
+++ views/home.php
@@ -6,11 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Projeto</title>
     <link rel="stylesheet" href="<?=URL?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   </head>
   <body>
    
   <div class="container">
-		<h1>ola</h1>
+  	<div class="row">
+      <div class="col-12">
+        <nav class="navbar navbar-light bg-light">
+          <a class="navbar-brand">
+            <img src="<?=URL?>assets/images/onehost.jpg" height="80px">
+          </a>
+          <form class="form-inline">
+            <input class="form-control mr-sm-5" type="search" placeholder="O que você procura..." aria-label="Search" style="width: 400px;">
+          </form>
+          <div class="clearfix">
+            <a href="#" class="float-left mr-4">
+              <i class='fas fa-shopping-cart' style="font-size: 30px;"></i>
+            </a>
+            <a href="#" class="float-left mr-2">
+              <i class='fas fa-user mr-1' style="font-size: 30px;"></i>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>   
+  
 	</div>
 
 	<script type="text/javascript" src="<?=URL?>assets/js/jquery.js"></script>
