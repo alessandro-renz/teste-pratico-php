79c310bd247cae8b80145060cd3b7b9a8658d778
Alessandro <sandro0325@gmail.com>
2019-01-27 15:05:47 -0200

    trocado o titulo do site

--- views/home.php
+++ views/home.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Projeto</title>
+    <title>Projeto - trocado</title>
     <link rel="stylesheet" href="<?=URL?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?=URL?>assets/css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
